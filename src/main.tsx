dfc8a82a1ebdab77cc8e60886509ecc0b8fc255f
Youssof2025 <youssof.m.soliman@gmail.com>
2026-01-19 01:33:39 -0700

    navbar implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
 
@@ -7,4 +8,10 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
+)
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
